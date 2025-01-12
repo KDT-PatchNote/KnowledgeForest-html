f4fe9de192f9acb786bafcc85d828046f25c4332
HwangEojin <jhin.h8@gmail.com>
2025-01-13 01:59:32 +0900

    FEATURE : 20250113 황어지 파일 업로드시 파일명 표시기능 생성

--- webapp/asset/js/study/studylist-write.js
+++ webapp/asset/js/study/studylist-write.js
@@ -47,3 +47,27 @@ capacityInput.addEventListener("input", (e) => {
     e.target.value = 20;
   }
 });
+
+//파일 첨부시 파일명 표시
+
+const fileInput = document.querySelector("input[name='file']");
+const fileLabel = document.querySelector("#STUDYLIST-INPUT-FILELABEL");
+
+fileInput.addEventListener("change", (e) => {
+  const filename = e.target.files[0].name;
+  fileLabel.textContent = filename;
+});
+
+
+//뒤로 가기 버튼을 눌렀을 때 이전 페이지로 이동
+function goBack() {
+  window.history.back();
+}
+
+// 글 등록을 눌렀을 때  alert, 스터디 목록 페이지로 이동
+function updateStudy() {
+  if (confirm("스터디를 등록하시겠습니까?")) {
+    alert("스터디가 등록되었습니다.");
+    location.href = "./studylist.html";
+  }
+}
