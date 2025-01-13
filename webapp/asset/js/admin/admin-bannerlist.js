774eecc1fb0e3f106217830a48e1813125b1252e
soooohyeon <dyddyd310@gmail,com>
2025-01-13 17:53:21 +0900

    EDIT : 20250113 박수현 관리자 배너 관리 페이지 작업 중

--- webapp/asset/js/admin/admin-bannerlist.js
+++ webapp/asset/js/admin/admin-bannerlist.js
@@ -1,3 +1,22 @@
+// 각 배너의 ... 클릭 시
+const menuBtn = document.querySelectorAll('.adminBanner-img-menubtn');
+const menubtnWrap = document.querySelectorAll('.adminBanner-div-buttonswrap');
+menuBtn.forEach((btn, idx) => {
+
+  btn.addEventListener("click", () => {
+    // 열린 배너의 메뉴를 제외한 다른 메뉴는 전부 숨기기
+    menubtnWrap.forEach((item) => {
+      item.style.display = 'none';
+    });
+    // 클릭한 배너의 메뉴만 보이기
+    btn.nextElementSibling.style.display = 'flex';
+  });
+});
+
+menubtnWrap.forEach((item) => {
+  item.style.display = 'none';
+});
+
 // 삭제 버튼 클릭시
 function clickDeleteBtn(event) {
   // 확인, 취소 버튼 선택
