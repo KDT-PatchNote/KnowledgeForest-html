c0b5b9b9eecd834523ea47b3726378ed884f74e4
HwangEojin <jhin.h8@gmail.com>
2025-01-14 01:14:46 +0900

    EDIT 20250114 황어진 코드 충돌 병합 이후 커밋

--- webapp/asset/js/board/boardlistregistration.js
+++ webapp/asset/js/board/boardlistregistration.js
@@ -1,7 +1,6 @@
-
 //파일 첨부시 파일명 표시
 const fileInput = document.querySelector("input[name='file']");
-const fileLabel = document.querySelector("#STUDYLIST-INPUT-FILELABEL");
+const fileLabel = document.querySelector("#BOARD-INPUT-FILELABEL");
 
 fileInput.addEventListener("change", (e) => {
   const filename = e.target.files[0].name;
@@ -15,8 +14,35 @@ function goBack() {
 
 // 글 등록을 눌렀을 때  alert, 스터디 목록 페이지로 이동
 function updateBoard() {
-  if (confirm("게시글을 등록하시겠습니까?")) {
-    alert("스터디가 등록되었습니다.");
-    location.href = "./boardlist.html";
+  if (validateForm()) {
+    if (confirm("게시글을 등록하시겠습니까?")) {
+      alert("스터디가 등록되었습니다.");
+      location.href = "./boardlist.html";
+    }
   }
+}
+
+const titleInput = document.querySelector("input[name='boardlistName']");
+const descriptionInput = document.querySelector(
+  "textarea[name='boardDescription']"
+);
+/**
+ * 유효성 검사
+ * @return boolean
+ * @author 황어진
+ * @since 2025-01-14
+ *
+ */
+function validateForm() {
+  if (!titleInput.value.trim()) {
+    alert("제목을 입력해주세요.");
+    titleInput.focus();
+    return false;
+  }
+  if (!descriptionInput.value.trim()) {
+    alert("설명을 입력해주세요.");
+    descriptionInput.focus();
+    return false;
+  }
+  return true;
 }
