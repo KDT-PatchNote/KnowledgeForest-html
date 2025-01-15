9c024fcbf301fa0ff025a73f50f799da436fcec7
soooohyeon <dyddyd310@gmail,com>
2025-01-15 17:49:56 +0900

    EDIT : 20250115 박수현 관리자 메뉴명 및 댓글 관리 페이지 수정

--- webapp/asset/js/admin/admin-boadcommentlist.js
+++ webapp/asset/js/admin/admin-boadcommentlist.js
@@ -10,3 +10,23 @@ function clickDeleteBtn(event) {
     alert('삭제가 완료되었습니다.');
   }
 }
+
+// 댓글 내용 클릭시 댓글 내용 전체 보이기
+const commentContent = document.querySelectorAll('.commentcontent');
+commentContent.forEach((comment) => {
+  // 해당 코멘트의 전체를 보여주는 div 선택
+  let commentAll = comment.parentNode.nextElementSibling;
+  let allBtn = comment.childNodes[3];
+  comment.addEventListener("click", () => {
+    // 해당 클래스가 없다면 부여 / 있으면 제거
+    commentAll.classList.toggle('active');
+    allBtn.classList.toggle('rotateBtn');
+  });
+  
+  // 코멘트 글 눌러도 제거
+  commentAll.addEventListener("click", () => {
+    // 해당 클래스가 없다면 부여 / 있으면 제거
+    commentAll.classList.toggle('active');
+    allBtn.classList.toggle('rotateBtn');
+  });
+});
--- webapp/asset/js/admin/admin-studycommentlist.js
+++ webapp/asset/js/admin/admin-studycommentlist.js
@@ -16,14 +16,17 @@ const commentContent = document.querySelectorAll('.commentcontent');
 commentContent.forEach((comment) => {
   // 해당 코멘트의 전체를 보여주는 div 선택
   let commentAll = comment.parentNode.nextElementSibling;
+  let allBtn = comment.childNodes[3];
   comment.addEventListener("click", () => {
     // 해당 클래스가 없다면 부여 / 있으면 제거
     commentAll.classList.toggle('active');
+    allBtn.classList.toggle('rotateBtn');
   });
   
   // 코멘트 글 눌러도 제거
   commentAll.addEventListener("click", () => {
     // 해당 클래스가 없다면 부여 / 있으면 제거
     commentAll.classList.toggle('active');
+    allBtn.classList.toggle('rotateBtn');
   });
 });
